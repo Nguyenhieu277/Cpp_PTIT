7139958cd6c34d49a759423fc7adc87429c4468f
HiuDev <145979785+Nguyenhieu277@users.noreply.github.com>
2024-06-16 21:51:30 +0700

    Update day_so_bitonic.cpp

--- day_so_bitonic.cpp
+++ day_so_bitonic.cpp
@@ -11,36 +11,40 @@ const int mod = 1000000007;
 void Dp(vector<int> &v, int n){
     vector<int> left(n + 1), right(n + 1);
     for(int i = 0; i < n; i++){
-        left[i] = v[i];
+        left[i] = 1;
         for(int j = 0; j < i; j++){
             if(v[i] > v[j]){
-                left[i] = max(left[i], left[j] + v[i]);
+                left[i] = max(left[i], left[j] + 1);
             }
         }
     }
     for(int i = n - 1; i >= 0; i--){
-        right[i] = v[i];
+        right[i] = 1;
         for(int j = n - 1; j > i; j--){
             if(v[i] > v[j]){
-                right[i] = max(right[i], right[j] + v[i]);
+                right[i] = max(right[i], right[j] + 1);
             }
         }
     }
     int res = INT_MIN;
     for(int i = 0; i < n; i++){
-        res = max(res, left[i] + right[i] - v[i]);
+        res = max(res, left[i] + right[i] - 1);
     }
     cout << res << endl;
 }
 int main(){
     quick();
-    int n;
-    cin >> n;
-    vector<int> v;
-    for(int i = 0; i < n; i++){
-        int x; cin >> x;
-        v.push_back(x);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> v;
+        for(int i = 0; i < n; i++){
+            int x; cin >> x;
+            v.push_back(x);
+        }
+        Dp(v, n);
     }
-    Dp(v, n);
     return 0;
 }
